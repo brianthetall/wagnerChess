f2d01443a4d2c2ebb9eb155292d0be7682e9bd23
Brian Wagner <brianthetall@gmail.com>
2019-08-24 22:02:04 +0000

    added first test file

---
+++ wagnerChess/pytests/test_location.py
@@ -0,0 +1,13 @@
+import pytest
+import logging
+import sys
+import os
+
+
+def test_location_feature_0():
+
+    logger = logging.getLogger("logger")
+    logger.setLevel(logging.DEBUG)
+
+    assert (True)
+    
