4eb27c4fd259ae88af1d9d9a3d94345acdfcdb37
Brian@Home <brianthetall@gmail.com>
2019-08-25 10:15:11 -0400

    Run more jenkins

--- wagnerChess/jenkins/run_tests.sh
+++ wagnerChess/jenkins/run_tests.sh
@@ -3,10 +3,15 @@ set -x
 
 BACKEND=${BACKEND:="$WORKSPACE/games/wagnerChess"}
 
+pwd
+ls -laht
+echo $BACKEND
+echo $WORKSPACE
+
 source $WORKSPACE/.pyenv-usr-local-bin-python3.6/bin/activate
 source $BACKEND/setEnvVariables.sh
 
-python -m pytest ./pytests/test_location.py
+python -m pytest ./wagnerChess/pytests/test_location.py
 #python -m pytest ./pytests/test_.py
 #python -m pytest ./pytests/test_.py
 #python -m pytest ./pytests/test_.py
