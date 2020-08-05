7d68b86b38c084748dc6caf6aa7dcd7bd931047c
Brian Wagner <brianthetall@gmail.com>
2020-08-05 16:17:06 -0400

    Pawn needs double move on first move support.

--- wagnerChess/pawn.py
+++ wagnerChess/pawn.py
@@ -6,10 +6,80 @@ class Pawn(Piece):
 
         super(Pawn,self).__init__(location,root,"pawn",color)
 
+    def isEnemyPresent(self,location):
+        piece=location.getPiece()
+        if piece==None:
+            return None
+        elif piece.color!=self.color:
+            return location
+        else:
+            return None
 
     def listMoves(self):
-        pass
+        validMoves=[]
 
+        if self.color=="white":
+            while True:
+                #look NE
+                tempLoc=self.location.getNorthEast()
+                if tempLoc==None:
+                    break
+                tempLoc=self.checkNextLocation(tempLoc)
+
+                if tempLoc==None:
+                    break
+                else:
+                    validMoves.append(tempLoc)
+                    print("c")
+                    break
+                
+                
+            #look N for enemey pieces
+            tempLoc=self.location.getNorth()
+            enemyLoc=self.isEnemyPresent(tempLoc)
+
+            if enemyLoc!=None:
+                validMoves.append(enemyLoc)
+                print("appended enemyLoc")
+
+            #look E for enemey pieces
+            tempLoc=self.location.getEast()
+            enemyLoc=self.isEnemyPresent(tempLoc)
+
+            if enemyLoc!=None:
+                validMoves.append(enemyLoc)
+                print("East emenyLoc appended")
+
+                
+        elif self.color=="black":
+            #look SW
+            while True:
+                #look NE
+                tempLoc=self.location.getSouthWest()
+                if tempLoc==None:
+                    break
+                tempLoc=self.checkNextLocation(tempLoc)
+                if tempLoc==None:
+                    break
+                else:
+                    validMoves.append(tempLoc)
+                    break
+
+            #look S for enemey pieces
+            tempLoc=self.location.getSouth()
+            enemyLoc=self.isEnemyPresent(tempLoc)
+            if enemyLoc!=None:
+                validMoves.append(enemyLoc)
+
+            #look W for enemey pieces
+            tempLoc=self.location.getWest()
+            enemyLoc=self.isEnemyPresent(tempLoc)
+            if enemyLoc!=None:
+                validMoves.append(enemyLoc)
+                
+                
+
+        return validMoves
 
     def toString(self):
         return "p "
