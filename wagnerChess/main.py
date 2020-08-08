1870a32a124eba2f3863c7b4e95a7ce0c7f9497f
Brian Wagner <brianthetall@gmail.com>
2020-08-07 22:25:20 -0400

    Working on socket implementation.

--- wagnerChess/main.py
+++ wagnerChess/main.py
@@ -1,5 +1,31 @@
 from board import Board
 from notYourPieceException import NotYourPieceException
+import socket
+
+while True:
+    mode=input("(S)erver or (C)lient?")
+    if mode=="S" or mode=="s" or mode=="C" or mode=="c":
+        break
+    print ("Try again!")
+
+
+port=26002
+if mode=="s" or mode=="S":#server
+    #port=input("Port: ")
+    #port=26000
+    #open a server-socket
+    ssocket=socket.socket()
+    ssocket.bind(("0.0.0.0",port))
+    #print ("socket hostname:"+socket.gethostname())
+    ssocket.listen()
+    
+else:#client
+    #serverIp=input("Server IP:")
+    serverIp="127.0.0.1"
+    #port=input("Server Port:")
+    #port=26000
+    #open socket
+    socket=socket.socket()
 
 #main has board
 #board has players && Locations
@@ -13,7 +39,21 @@ while True:
 
     while True:
         try:
-            move=input("White Move: ")#location,location
+            if mode=="s" or mode=="S":
+                move=input("White Move: ")#location,location
+                #send move to the Client
+                c,addr=ssocket.accept()
+                print("Sending to IP:"+str(addr))
+                c.send(move)
+                c.close()
+                
+            else:
+                #recv from Server player
+                socket.connect((serverIp,port))
+                move=socket.recv(1024)
+                print("Move from server: "+move)
+                socket.close()
+                
             #check for valid move
             b.movePiece(move,color="white")#execute update to board
             print(b.toString())
@@ -30,7 +70,21 @@ while True:
 
     while True:
         try:
-            move=input("Black Move: ")
+            if mode=="c" or mode=="C":
+                move=input("Black Move: ")
+
+                #send move to the Server
+                socket.connect((serverIp,port))
+                socket.send(move)
+                socket.close()
+                
+            else:#Recv move from Client
+                c,addr=ssocket.accept()
+                print("Move from IP:"+(addr))
+                move=c.recv()
+                print("Move from client:"+move)
+                c.close()
+                
             b.movePiece(move,color="black")#execute update to board
             print(b.toString())
             break
